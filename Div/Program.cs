71bf4a421b448d6464e2fec20f0d73d9c4feceab
Karina <Karishka.1996@mail.ru>
2023-01-20 10:11:40 +0400

    Div

---
+++ Div/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+
+int NumberA = Convert.ToInt32(Console.ReadLine());
+int Div = NumberA % 2;
+
+if(Div == 0)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+  Console.WriteLine("Нет");  
+}
