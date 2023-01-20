517a0c9bb4a50438d58a995263fa11c2cf38efb9
Karina <Karishka.1996@mail.ru>
2023-01-20 10:31:41 +0400

    Even number

---
+++ EvenNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+
+int NumberA = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 2;
+
+if(NumberA > 1)
+{
+    while(evenNumber <= NumberA)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
